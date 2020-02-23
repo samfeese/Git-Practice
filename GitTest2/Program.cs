c66d2f1c50bfcc8ea24cd3c2d185e5fb35426a29
Samuel Feese <samuelfeese@outlook.com>
2020-02-23 14:01:07 -0500

    commit 3

---
+++ GitTest2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestNamespace;
+
+namespace GitTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass(2, 4);
+
+            Console.WriteLine(testClass.number1);
+            Console.WriteLine(testClass.number2);
+
+            
+        }
+    }
+}
---
+++ GitTest2/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNamespace
+{
+    public class TestClass
+    {
+        public int number1;
+        public int number2;
+        public TestClass(int num1, int num2)
+        {
+            number1 = num1;
+            number2 = num2;
+
+        }
+    }
+}
